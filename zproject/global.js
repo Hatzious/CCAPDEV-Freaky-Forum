fd76aa70ca55aeb8059f053c0c58dea3713c57a1
Hatzious <avtiu29@gmail.com>
2026-02-12 23:00:50 +0800

    feat: preparation of global html now ready

--- zproject/global.js
+++ zproject/global.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
     var music = document.getElementById("bgm");
-    var isPlaying = false;
+    button = document.getElementById("music-toggle").innerHTML = "🔊";
+    var isPlaying = !music.paused;
 
     music.volume = 0.2;
-    document.getElementById("music-toggle").innerHTML = "🔊";
-
     $("#music-toggle").click(function() {
         if (isPlaying) {
             music.pause();
