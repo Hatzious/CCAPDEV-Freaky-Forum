3a520e622431ea45ea88fabdd497868ade62e16a
Hatzious <avtiu29@gmail.com>
2026-02-16 18:49:28 +0800

    chore: setup of techstack, react, express, and tailwind is now in effect.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./Freaky Archives/src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./Freaky Archives/index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
