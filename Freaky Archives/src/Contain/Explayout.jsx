024aa796e2989df400ffe6f3c8b11084876cd0b1
dmdce <da.real.esguerra@gmail.com>
2026-03-02 10:19:26 +0800

    feat: changed bg into gradient

--- Freaky Archives/src/Contain/Explayout.jsx
+++ Freaky Archives/src/Contain/Explayout.jsx
@@ -1,6 +1,6 @@
 export default function Explayout({ children, classes="" }) {
     return (
-        <div className={`min-h-screen w-full relative bg-black no-underline flex ${classes}`}>
+        <div className={`min-h-screen w-full relative bg-[radial-gradient(circle,var(--color-accent-dark-2)_0%,var(--color-accent-dark-1)_90%)] no-underline flex ${classes}`}>
             {children}
         </div>
     );
