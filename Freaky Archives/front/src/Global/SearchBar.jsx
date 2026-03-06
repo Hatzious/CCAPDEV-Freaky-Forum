f513ee53aa7ad412853a06703a2f8b1cf784f6de
Hatzious <avtiu29@gmail.com>
2026-03-06 20:20:26 +0800

    clean: removed auth and login from the searchbar component

--- Freaky Archives/front/src/Global/SearchBar.jsx
+++ Freaky Archives/front/src/Global/SearchBar.jsx
@@ -2,12 +2,6 @@ import { useNavigate } from "react-router-dom";
 import { useAuth } from "../Services/Auth";
 
 export default function SearchBar({ onClose, classes = "visible" }) {
-    const { logout } = useAuth();
-    const navigate = useNavigate();
-    const loggingout = () => {
-        logout();
-        navigate("/");
-    };
 
     const HoverStyles = ` -ml-10 flex z-2 w-150 h-10 bg-olive border-1 border-solid border-border 
                          items-center justify-center top-full right-0 blur-none`;
