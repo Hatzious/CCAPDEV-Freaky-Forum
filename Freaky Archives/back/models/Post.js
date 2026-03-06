0d523ed0056bd8579eec42804ccce6530d2572a0
Hatzious <avtiu29@gmail.com>
2026-03-06 20:15:51 +0800

    feat: progress setup on forum post filtering feature

---
+++ Freaky Archives/back/models/Post.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    tags: {
+    type: [{ 
+        type: String, 
+        lowercase: true 
+    }],
+    default: ["empty"]},
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    score: {
+        type: Number,
+        default: 0
+    },
+    views: {
+        type: Number,
+        default: 0
+    }
+}, { timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }});
+
+module.exports = mongoose.model('Post', PostSchema);
