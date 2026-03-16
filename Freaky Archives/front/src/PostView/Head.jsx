d1c72fa0e5fbc08742d4ebe4c801140a202e3654
dmdce <da.real.esguerra@gmail.com>
2026-03-17 00:14:54 +0800

    feat: initial structure of PostView

---
+++ Freaky Archives/front/src/PostView/Head.jsx
@@ -0,0 +1,20 @@
+import Click from "../Global/Click";
+import Info from "../Forum/Info";
+import Tags from "../Forum/Tags";
+
+export default function Head({
+    title = "",
+    author = "",
+    date = "",
+    tags = []
+}) {
+    return (
+        <div className="flex flex-col bg-olive h-auto w-full border-border border-post items-start py-6 px-6 gap-y-4 mb-4">
+            <div className="flex flex-col grow gap-y-2 h-full justify-center break-words">
+                <Click label={title} size="text-small" />
+                <Info author={author} date={date} />
+                <Tags tags={tags} />
+            </div>     
+        </div>
+    );
+}
--- Freaky Archives/front/src/PostView/PostView.jsx
+++ Freaky Archives/front/src/PostView/PostView.jsx
@@ -1,4 +1,4 @@
-import Post from "../Post/Post";
+import Head from "./Head";
 import Body from "./Body";
 import Comments from "./Comments";
 
@@ -6,7 +6,7 @@ export default function PostView() {
     return (
         <div className="flex flex-row justify-center">
             <div className="w-7/12 h-screen flex flex-col justify-start">
-                {/* <Title /> */}
+                <Head title="title" author="author" date="00/00/0000" tags={["tag1","tag2"]} />
                 <Body />
                 <Comments />
             </div>
