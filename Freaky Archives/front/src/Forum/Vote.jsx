cd48b6536fccde8e475024ec05b00fa4ae03d61e
Hatzious <avtiu29@gmail.com>
2026-03-19 23:00:12 +0800

    fix: vote display was stuck at 0

--- Freaky Archives/front/src/Forum/Vote.jsx
+++ Freaky Archives/front/src/Forum/Vote.jsx
@@ -1,11 +1,11 @@
 import Arrow from "./Arrow";
 import VoteTrack from "./VoteTrack";
 
-export default function Vote() {
+export default function Vote({ score=0 }) {
     return (
         <div className="flex grow-2 h-full flex-col items-end justify-center">
             <Arrow />
-            <VoteTrack />
+            <VoteTrack initial={score} />
             <Arrow direction="down" color="var(--color-downvote)"/>
         </div>
     );
