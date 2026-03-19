edbfc2540ed48b0c908ad85bf57e27033a446ce0
rizel <rozette.dominique@gmail.com>
2026-03-19 22:30:42 +0800

    center a div

--- Freaky Archives/front/src/Forum/PostButton.jsx
+++ Freaky Archives/front/src/Forum/PostButton.jsx
@@ -2,8 +2,8 @@ import Click from "../Global/Click";
 
 export default function PostButton() {
     return (
-        <div className="flex w-[8vw] h-[5vh] mb-5 -mt-5 bg-olive border border-border 
-                        self-end items-center justify-center font-french-canon 
+        <div className="flex w-[8vw] h-[5vh] pl-3 mb-5 -mt-5 bg-olive border border-border 
+                        self-end items-center justify-end font-french-canon 
                         hover:text-shadow-glow hover:brightness-90 duration-300">
             <Click label="Create Post" to="/createpost" size="text-xxxs" bordered={true} post="true"/>
         </div>
