9caa0505caf5201bff393eae6e892c44a6221767
rizel <rozette.dominique@gmail.com>
2026-04-02 16:29:43 +0800

    The Philippines, officially the Republic of the Philippines, is an archipelagic country in Southeast Asia. Located in the western Pacific Ocean, it consists of about 7,641 islands, with a total area of about 300,000 square kilometers, which are broadly categorized in three main geographical divisions from north to south: Luzon, Visayas, and Mindanao. With a population of over 112 million, it is the world's fourteenth-most-populous country. (Make eyes in the welcome page)

---
+++ Freaky Archives/front/src/Welcome/EyesBlinking.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+
+export default function EyesBlinking() {
+    const [isOpen, setIsOpen] = useState(true);
+    const [randomStyle, setRandomStyle] = useState({});
+
+    useEffect(() => {
+      const randomX = Math.floor(Math.random() * 16 - 8); 
+      const randomY = Math.floor(Math.random() * 20 - 10); 
+      const randomScale = 0.8 + Math.random() * 0.5;      
+      const randomRotate = Math.floor(Math.random() * 20 - 10); 
+
+      setRandomStyle({
+          transform: `translate(${randomX}px, ${randomY}px) scale(${randomScale}) rotate(${randomRotate}deg)`,
+      });
+
+      const blink = () => {
+        setIsOpen(false);
+
+        setTimeout(() => {setIsOpen(true);}, 1500);
+
+        const nextBlink = Math.random() * 5000 + 3000;
+        setTimeout(blink, nextBlink);
+      };
+
+      const initialTimeout = setTimeout(blink, Math.random() * 3000);
+      return () => clearTimeout(initialTimeout);
+    }, []);
+
+  return (
+      <img 
+        src={isOpen ? "/pic/OpenedEye.png" : "/pic/ClosedEye.png"} 
+        alt="Eye"
+        style={randomStyle}
+        className="w-full h-full object-cover transition-opacity duration-100"
+      />
+  );
+}
--- Freaky Archives/front/src/Welcome/Welcome.jsx
+++ Freaky Archives/front/src/Welcome/Welcome.jsx
@@ -1,11 +1,19 @@
 import Logo from "../Global/Logo";
 import Click from "../Global/Click";
+import EyesBlinking from "./EyesBlinking";
 import Explayout from "../Contain/Explayout";
 
 export default function Welcome() {
     return (
-        <Explayout classes="items-center justify-center pb-8">
-            <div className="flex flex-col items-center gap-y-2">
+        <Explayout classes="items-center justify-center pb-8 h-full overflow-hidden">
+            <div className="absolute inset-0 grid grid-cols-32 opacity-20">
+            {Array.from({ length: 512 }).map((_, index) => (
+                <div key={index} className="col-span-1 row-span-1">
+                    <EyesBlinking />
+                </div>
+            ))}
+            </div>
+            <div className="relative flex flex-col items-center gap-y-2 z-10">
                 <Logo size="text-xl" />
                 <Click label="Make your statement. Face your fear."
                 size="text-small" to="/forum"
