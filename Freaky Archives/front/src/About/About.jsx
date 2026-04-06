dd3bbdc7695dd73a2a62e2f316db3bae632c4731
dmdce <da.real.esguerra@gmail.com>
2026-04-06 18:10:48 +0800

    update: about

--- Freaky Archives/front/src/About/About.jsx
+++ Freaky Archives/front/src/About/About.jsx
@@ -13,11 +13,12 @@ export default function About() {
 
                     <HBar text="ABOUT" />
                     <p className="text-glow text-center font-french-canon">
-                        This forum was developed with inspiration from Magnus Archives, a series of horror stories created by a group called <a href="https://rustyquill.com/show/the-magnus-archives/" className="text-secondary-1 hover:underline">Rusty Quill</a>. This forum website exists to provide a community around sharing and investigating horror stories. Most existing websites serve as non-persistent discussion boards even if the content revolves around horror fiction, but Freaky Archives provide a centralized venue for users to contribute to a repository of user-made horror stories (a.k.a creepy pasta) for everyone to access.
+                        This forum was developed with inspiration from Magnus Archives, a series of horror stories created by a group called <a target="_blank" rel="noreferrer noopener" href="https://rustyquill.com/show/the-magnus-archives/" className="text-secondary-1 hover:underline">Rusty Quill</a>. This forum website exists to provide a community around sharing and investigating horror stories. Most existing websites serve as non-persistent discussion boards even if the content revolves around horror fiction, but Freaky Archives provide a centralized venue for users to contribute to a repository of user-made horror stories (a.k.a creepy pasta) for everyone to access.
                     </p>
                     <p className="text-glow text-center font-french-canon">
                         Link to website source code: <a target="_blank" rel="noreferrer noopener" href="https://github.com/Hatzious/CCAPDEV-Freaky-Forum" className="text-secondary-1 hover:underline">Freaky Archives</a>
                     </p>
+
                     <HBar text="CREATORS" />
                     <div className="flex gap-x-16 text-center">
                         <ProfileTile name="Aya-ay, Viktor Rafale" pic="/creators/Rafale_Photo.jpg" />
@@ -26,20 +27,37 @@ export default function About() {
                         <ProfileTile name="Tiu, Avram Nathaniel" pic="/creators/Tiu.jpg" />
                     </div>
 
-                    <HBar text="FRAMEWORKS & PACKAGES" />
-                    <div className="flex gap-x-64 font-french-canon">
+                    <HBar text="FRAMEWORKS & LIBRARIES" />
+                    <div className="flex gap-x-32 font-french-canon">
                         <ul className="text-glow list-disc list-inside">
-                            <li>Node.js</li>
-                            <li>MongoDB</li>
-                            <li>MongoDB Atlas</li>
-                            <li>Mongoose</li>
-                            <li>Express</li>
-                            <li>Nodemon</li>
+                            <li>Express.js <span className="text-secondary-1">by OpenJS Foundation</span></li>
+                            <li>MongoDB <span className="text-secondary-1">by MongoDB Inc.</span></li>
+                            <li>MongoDB Atlas <span className="text-secondary-1">by MongoDB Inc.</span></li>
+                            <li>Node.js <span className="text-secondary-1">by OpenJS Foundation</span></li>
+                            <li>Tailwind CSS <span className="text-secondary-1">by Tailwind Labs</span></li>
+                            <li>Vite <span className="text-secondary-1">by Evan You</span></li>
                         </ul>
 
                         <ul className="text-glow list-disc list-inside">
-                            <li>react-outside-click-handler by airbnb</li>
-                            <li>canvas-confetti by catdad</li>
+                            <li>mongoose <span className="text-secondary-1">by Automattic</span></li>
+                            <li>react <span className="text-secondary-1">by Meta</span></li>
+                            <li>react-dom <span className="text-secondary-1">by Meta</span></li>
+                            <li>react-markdown <span className="text-secondary-1">by remarkjs</span></li>
+                            <li>react-router-dom <span className="text-secondary-1">by Remix Software</span></li>
+                            <li>react-swipeable <span className="text-secondary-1">by FormidableLabs</span></li>
+                        </ul>
+                    </div>
+
+                    <HBar text="PACKAGES" />
+                    <div className="flex gap-x-64 font-french-canon justify-center">
+                        <ul className="text-glow list-disc list-inside">
+                            <li>bcrypt <span className="text-secondary-1">by kelektiv</span></li>
+                            <li>canvas-confetti <span className="text-secondary-1">by catdad</span></li>
+                            <li>connect-mongo <span className="text-secondary-1">by jdesboeufs</span></li>
+                            <li>dotenv <span className="text-secondary-1">by motdotla</span></li>
+                            <li>react-outside-click-handler <span className="text-secondary-1">by airbnb</span></li>
+                            <li>nodemon <span className="text-secondary-1">by Remy Sharp</span></li>
+                            <li>cors <span className="text-secondary-1">by Express.js</span></li>
                         </ul>
                     </div>
 
