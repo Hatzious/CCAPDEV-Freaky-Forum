5a2ef8df6860bfa79e9eb3a56d81239f350e4e35
Hatzious <avtiu29@gmail.com>
2026-04-06 16:50:54 +0800

    feat: included github link to source code in about page

--- Freaky Archives/front/src/About/About.jsx
+++ Freaky Archives/front/src/About/About.jsx
@@ -15,7 +15,9 @@ export default function About() {
                     <p className="text-glow text-center font-french-canon">
                         This forum was developed with inspiration from Magnus Archives, a series of horror stories created by a group called <a href="https://rustyquill.com/show/the-magnus-archives/" className="text-secondary-1 hover:underline">Rusty Quill</a>. This forum website exists to provide a community around sharing and investigating horror stories. Most existing websites serve as non-persistent discussion boards even if the content revolves around horror fiction, but Freaky Archives provide a centralized venue for users to contribute to a repository of user-made horror stories (a.k.a creepy pasta) for everyone to access.
                     </p>
-
+                    <p className="text-glow text-center font-french-canon">
+                        Link to website source code: <a target="_blank" rel="noreferrer noopener" href="https://github.com/Hatzious/CCAPDEV-Freaky-Forum" className="text-secondary-1 hover:underline">Freaky Archives</a>
+                    </p>
                     <HBar text="CREATORS" />
                     <div className="flex gap-x-16 text-center">
                         <ProfileTile name="Aya-ay, Viktor Rafale" pic="/creators/Rafale_Photo.jpg" />
