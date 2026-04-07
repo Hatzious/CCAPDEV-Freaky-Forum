76f0f0d864f47c09afe5fd3167a83c1b66bebd84
Hatzious <avtiu29@gmail.com>
2026-04-07 16:55:07 +0800

    fix: line breaking issue on postview now recognized

--- Freaky Archives/front/src/PostView/Body.jsx
+++ Freaky Archives/front/src/PostView/Body.jsx
@@ -1,6 +1,7 @@
 import ReactMarkdown from 'react-markdown';
 import Click from "../Global/Click";
 import remarkGfm from 'remark-gfm';
+import remarkBreaks from 'remark-breaks';
 
 export default function Body({text = "oops body issue", onQuote, postData, id}) {
     return (
@@ -9,7 +10,7 @@ export default function Body({text = "oops body issue", onQuote, postData, id}) 
                 className="text-glow font-french-canon text-xxxs break-words max-w-full overflow-hidden"
             >
                 <ReactMarkdown
-                    remarkPlugins={[remarkGfm]} 
+                    remarkPlugins={[remarkGfm, remarkBreaks]} 
                     components={{
                         p: ({node, ...props}) => <p className="mb-4 last:mb-0 leading-relaxed" {...props} />
                     }}
