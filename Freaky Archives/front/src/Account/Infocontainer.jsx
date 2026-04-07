bd3a24d165083c8e078a7abc0e3d7178881722b6
dmdce <da.real.esguerra@gmail.com>
2026-04-07 14:49:02 +0800

    nothing to see here

--- Freaky Archives/front/src/Account/Infocontainer.jsx
+++ Freaky Archives/front/src/Account/Infocontainer.jsx
@@ -11,11 +11,9 @@ export default function Infocontainer() {
         <div className="flex w-full justify-between gap-x-2 pr-[30%]">
             <div className="flex flex-col gap-y-1">
                 <Prettytext fontStyle="font-varela">Joined: { formattedDate }</Prettytext>
-                <Prettytext fontStyle="font-varela">Followers: { user.stats.followerCount }</Prettytext>
             </div>
             <div className="flex flex-col gap-y-1">
                 <Prettytext fontStyle="font-varela">Last Active: { formattedTime }</Prettytext>
-                <Prettytext fontStyle="font-varela">Following: { user.stats.followingCount }</Prettytext>
             </div>
         </div>
     );
